f7645f308e8e92786d961687c676c03303c0875d
Ruslan Kabatsayev <b7.10110111@gmail.com>
2018-02-23 01:19:22 +0300

    Include f2c.h only at the very beginning of src.cpp

--- make-c-cat-friendly-and-cat.sh
+++ make-c-cat-friendly-and-cat.sh
@@ -21,12 +21,14 @@ cd ..
 # NOTE: converting dlamch_ declaration to dlamch_DISABLED instead of removing
 # it because sometimes it appears in the same line as another declaration,
 # which we don't want to remove.
+echo '#include "f2c.h"' > src.cpp
 cat "$dirName"/*.c | sed -e 's@\<char *\*@const char*@g' \
                          -e 's@\<TRUE_\>@ true @g' \
                          -e 's@\<FALSE_\>@ false @g' \
                          -e '/#ifdef __cplusplus/,/#endif$/d' \
+                         -e '/#include "f2c.h"/d' \
                          -e '/disnan_(doublereal/d' \
                          -e 's@extern\(.*\)\<dlamch_(@extern\1dlamch_DISABLED(@' \
                          -e 's@dlamch_("@dlamch_<doublereal>("@g' \
                          -e '/^ *\/\* Subroutine \*\/.*;$/d' \
-                         -e '/^ *\/\* Builtin functions \*\/$/,/^$/d' > src.cpp
+                         -e '/^ *\/\* Builtin functions \*\/$/,/^$/d' >> src.cpp
